14ec45a2e96e165ce8743eea41acbf72f21c858d
ryangholland <rghcode@gmail.com>
2023-05-28 19:52:40 -0400

    Create Task and Project factories, dataController module

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Hello JS");
+(()=>{let s=((s,a,e,t)=>{let l=!1;return{displayProps:()=>{console.log({title:"Default Task",description:"A default task.",dueDate:"Tomorrow",priority:"High",completed:l})},completeTask:()=>{l=!l}}})(),a=(s=>{let a=[];return{addTask:s=>{a.push(s)},displayTasks:()=>{a.forEach((s=>{s.displayProps()}))}}})();a.addTask(s),a.displayTasks()})();
