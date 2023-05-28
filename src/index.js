d57677e4ca6ec4e8c6ddc7269332125f732cab0d
ryangholland <rghcode@gmail.com>
2023-05-28 19:52:40 -0400

    Create Task and Project factories, dataController module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-console.log("Hello JS")
+const Task = (title, description, dueDate, priority) => {
+  let completed = false;
+
+  const completeTask = () => {
+    completed = !completed;
+  };
+
+  const displayProps = () => {
+    console.log({ title, description, dueDate, priority, completed });
+  };
+
+  return { displayProps, completeTask };
+};
+
+const Project = (title) => {
+  let tasks = [];
+
+  const addTask = (task) => {
+    tasks.push(task);
+  };
+
+  const displayTasks = () => {
+    tasks.forEach((task) => {
+      task.displayProps();
+    });
+  };
+
+  return { addTask, displayTasks };
+};
+
+const dataController = (() => {
+  const projects = [];
+
+  const createProject = (title) => {
+    let newProject = Project(title);
+    projects.push(newProject);
+  };
+
+  const loadProjects = () => {
+    // Get projects from LOCALSTORAGE if available; if not, create a default project
+
+    let defaultTask = Task("Default Task", "A default task.", "Tomorrow", "High");
+    let defaultProject = Project("Default Project");
+    defaultProject.addTask(defaultTask);
+    defaultProject.displayTasks();
+  }
+
+  loadProjects();
+
+  return { createProject };
+})();
+
+const inputController = (() => {})();
+
+const displayController = (() => {})();
+
+
